b0e1b168c450444fb58929230dc6f1c16b028da5
Nam Thanh Nguyen <35841468+jkelol111@users.noreply.github.com>
2020-10-15 20:54:55 +0800

    Update RevisionExercise4.vb to human numbering lol

--- vb/RevisionExercise4.vb
+++ vb/RevisionExercise4.vb
@@ -1,16 +1,16 @@
 Module RevisionExercise4
 	Sub Main()
 		Dim i, j As Integer
-		i = 0
-		j = 0
+		i = 1
+		j = 1
 
-		While (i <= 4)
-			While (j <= 4)
+		While (i <= 5)
+			While (j <= 5)
 				Console.Write("* ")
 				j += 1
 			End While
 			i += 1
-			j = 0
+			j = 1
 			Console.WriteLine()
 		End While
 
